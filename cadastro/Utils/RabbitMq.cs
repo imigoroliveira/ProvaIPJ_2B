2d384030582e4dacc7b34fa93289a40011b3b7d2
Jorge-Neto <neto_tunas@hotmail.com>
2022-06-28 20:41:15 -0300

    Using RabbitMQ

---
+++ cadastro/Utils/RabbitMq.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using cadastro.Model;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace cadastro.Utils
+{
+    class Send
+    {
+        public static void Main(FolhaModel newFolha)
+        {
+            FolhaModel folha = newFolha;
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "cadastrofolha",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string jsonString = JsonSerializer.Serialize(folha);
+                var body = Encoding.UTF8.GetBytes(jsonString);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", body);
+            }
+
+        }
+    }
+
+}
